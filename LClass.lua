3ebc252e839424365099e41d7eb2dce90355a84f
kirbybaobao <kirbybaobao@gmail.com>
2020-01-26 23:15:31 +0800

    create class

--- LClass.lua
+++ LClass.lua
@@ -1,4 +1,26 @@
 local LClass = {}
 
+local aClassMeta =
+{
+    __tostring = function(aClass)
+        return "CLASS: " .. aClass.name
+    end,
+}
+local function _createClass(name, super)
+    local aClass = {}
+    setmetatable(aClass, aClassMeta)
+    aClass.name = name
+
+    return aClass
+end
+
+local meta =
+{
+    __call = function(aClass, name, super)
+        -- assert
+        return _createClass(name, super)
+    end,
+}
+setmetatable(LClass, meta)
 
 return LClass
---
+++ test.lua
@@ -0,0 +1,4 @@
+local LClass = dofile "LClass.lua"
+
+local C1 = LClass("C1")
+print("C1", C1)
